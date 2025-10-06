b4c2ae8412aa84d0dbda92469bb9d4be9224e85a
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-10-05 21:22:09 -0300

    switch colorscheme to Catppuccin Mocha

--- appearance.lua
+++ appearance.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-	config.color_scheme = "Dracula"
+	config.color_scheme = "Catppuccin Mocha"
 
 	config.font_dirs = { "fonts" }
 	config.font_locator = "ConfigDirsOnly"
